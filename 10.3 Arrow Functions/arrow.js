7d966bdd712d1eae3bb96cbb7eb6993cf997137f
Lordkarma88 <calvin.hg.lamb@yandex.com>
2022-11-26 12:34:25 -0600

    unit 10.3

---
+++ 10.3 Arrow Functions/arrow.js
@@ -0,0 +1,29 @@
+//ES5 Map Callback
+function double(arr) {
+  return arr.map(function (val) {
+    return val * 2;
+  });
+}
+
+// ES2015 Arrow Functions Shorthand
+// Refactor the above code to use two arrow functions. Turn it into a one-liner.
+
+const dbl = (arr) => arr.map((val) => val * 2);
+
+// Refactor the following function to use arrow functions:
+// Replace ALL functions with arrow functions:
+
+function oldSquareAndFindEvens(numbers) {
+  var squares = numbers.map(function (num) {
+    return num ** 2;
+  });
+  var evens = squares.filter(function (square) {
+    return square % 2 === 0;
+  });
+  return evens;
+}
+
+const squareAndFindEvens = (numbers) => {
+  const squares = numbers.map((num) => num ** 2);
+  return squares.filter((num) => num % 2 === 0);
+};
