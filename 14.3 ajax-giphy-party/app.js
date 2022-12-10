8d1d7b23c76530959f2c602aca34af3411945c19
Lordkarma88 <calvin.hg.lamb@yandex.com>
2022-12-10 16:41:13 -0600

    unit 14.3

--- 14.3 ajax-giphy-party/app.js
+++ 14.3 ajax-giphy-party/app.js
@@ -1,1 +1,45 @@
 console.log("Let's get this party started!");
+
+$("#removeBtn").on("click", (evt) => {
+  evt.preventDefault();
+
+  for (let gif of $("#gifs").children()) {
+    gif.remove();
+  }
+});
+
+$("#searchBtn").on("click", (evt) => {
+  evt.preventDefault();
+
+  // Get tag to search with
+  const searchTag = $("#searchInput").val();
+  $("#searchInput").val(""); // Empty form
+
+  addGif(searchTag);
+});
+
+// Gets new gif and appends to #gifs
+const addGif = async (tag) => {
+  try {
+    const api_key = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+    const params = { params: { tag, api_key } };
+    const data = await axios.get(
+      "https://api.giphy.com/v1/gifs/random",
+      params
+    );
+    // Downsized image to fit better
+    const url = data.data.data.images.downsized.url;
+
+    // Make new img el with url found from data
+    const newGif = $("<img>")
+      .addClass("img-fluid")
+      .attr({ width: "100%", src: url });
+    // Make new column el and put image in it
+    const newCol = $("<div>").addClass("col-4 text-center p-1").append(newGif);
+    // Append col to #gifs
+    $("#gifs").append(newCol);
+  } catch (error) {
+    alert("Gif not found!");
+    console.log(error);
+  }
+};
