05f36c65a0a8315ba071dddcaa5787f11e60aa74
Lordkarma88 <calvin.hg.lamb@yandex.com>
2023-01-01 18:15:26 -0600

    removed dual survey functionality

--- S2_py_servers/19.3_flask-survey/app.py
+++ S2_py_servers/19.3_flask-survey/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import surveys
+from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
@@ -12,15 +12,11 @@ responses = []
 
 @app.route("/")
 def home():
-    return render_template("home.html", surveys=surveys)
+    return render_template("home.html", survey=survey)
 
 
-@app.route("/<survey_title>/q/<int:n>")
-def question_page(survey_title, n):
-    question = surveys[survey_title].questions[n-1]
+@app.route("/q/<int:n>")
+def question_page(n):
+    question = survey.questions[n-1]
 
     return render_template("question.html", q=question, n=n)
-
-
-@app.route("/answer")
-def answer()
